08960473e710cd22923ab5086daf6e0499568b27
nour-aldin <nourdx73@gmail.com>
2022-12-10 00:45:07 +0200

    hgklsdgfhsdlkfdshfl

--- src/components/UserManeger/SignIn.vue
+++ src/components/UserManeger/SignIn.vue
@@ -5,6 +5,7 @@
     <b-button variant="danger">Button</b-button>
     <b-button variant="success">Button</b-button>
     <b-button variant="outline-primary">Button</b-button>
+    <div></div>
   </div>
 </template>
 
