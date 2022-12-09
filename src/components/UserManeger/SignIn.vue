a061788144afae30b378e768eeb43c94b8a82d91
nour-aldin <nourdx73@gmail.com>
2022-12-10 00:43:56 +0200

    vueBootStrapAdded

---
+++ src/components/UserManeger/SignIn.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    {{ msg }}
+    <b-button>Button</b-button>
+    <b-button variant="danger">Button</b-button>
+    <b-button variant="success">Button</b-button>
+    <b-button variant="outline-primary">Button</b-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SignIn",
+  data: function () {
+    return {
+      msg: "This is sign in",
+    };
+  },
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <SignIn />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import SignIn from "@/components/UserManeger/SignIn.vue";
 
 export default {
   name: "HomeView",
   components: {
-    HelloWorld,
+    SignIn,
   },
 };
 </script>
