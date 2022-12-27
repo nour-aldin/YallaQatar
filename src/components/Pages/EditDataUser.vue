28a3a27c95cd3a42cb02a06a54eb1b854985c56c
AhmedMahmoudHafez <93212160+AhmedMahmoudHafez@users.noreply.github.com>
2022-12-28 00:04:21 +0200

    update F8

--- src/components/Pages/EditDataUser.vue
+++ src/components/Pages/EditDataUser.vue
@@ -1,19 +1,19 @@
 <template>
 <div class="main">
-<b-container>
+<b-container class="cnt" >
     <b-row>
         <b-col></b-col>
-        <b-col class="hm">
-            <div class="Form">
-                <b-form @submit.prevent="onSubmit" @reset="onReset">
+        <b-col class="hmm" cols="8" >
+            <div class="Formm">
+                <b-form   @submit.prevent="onSubmit" @reset="onReset">
                     <b-form-group id="input-group-1" label="First Name" label-for="input-1">
                         <b-form-input id="input-1" v-model="user.fisrtName" type="text" placeholder="update first name"
                             required></b-form-input>
                     </b-form-group>
-                    <b-form-group id="input-group-1" label="Second Name" label-for="input-2">
-                        <b-form-input id="input-1" v-model="user.secondName" type="text" placeholder="update second name" required></b-form-input>
+                    <b-form-group id="input-group-2" label="Second Name" label-for="input-2">
+                        <b-form-input id="input-2" v-model="user.secondName" type="text" placeholder="update second name" required></b-form-input>
                     </b-form-group>
-                    <b-form-group id="input-group-2" label="Password" label-for="input-3">
+                    <b-form-group id="input-group-3" label="Password" label-for="input-3">
                         <b-form-input v-model="user.password" :state="validation" id="feedback-user"
                             placeholder="Password" type="password">
                         </b-form-input>
@@ -24,9 +24,18 @@
                             Looks Good.
                         </b-form-valid-feedback>
                     </b-form-group>
-                    <b-button class="btn" type="submit" variant="success">Update</b-button>
+                    <b-form-group id="input-group-4" label="Birth Date" label-for="input-4">
+                        <b-form-input id="input-4" v-model="user.birthDate" placeholder="Birth Date" required type="date"></b-form-input>
+                    </b-form-group>
+                    <b-form-group id="input-group-5" label="Nationality" label-for="input-5">
+                        <b-form-input id="input-5" v-model="user.nationality" placeholder="Your Nationality" required></b-form-input>
+                    </b-form-group>
+                    <b-form-group id="input-group-6" label="Gender" label-for="input-6">
+                        <b-form-select id="input-6" v-model="user.gender" :options="genderList" required></b-form-select>
+                    </b-form-group>
+                    <b-button class="btnn" type="submit" variant="success">Update</b-button>
                 </b-form>
-            </div>
+                </div>
         </b-col>
         <b-col></b-col>
     </b-row>
@@ -44,23 +53,32 @@ export default {
                 fisrtName: "",
                 secondName:"",
                 password: "",
+                birthDate:"",
+                nationality:"",
+                gender:""
             },
             background: "@/assets/Icons/signup.webp",
+            genderList: [{ text: "Your Gender", value: null }, "male", "female"],
+
         };
     },
     methods: {
         onSubmit(event) {
             event.preventDefault();
-            let fisrtName = this.user.fisrtName;
+            let firstName = this.user.fisrtName;
             let secondName = this.user.secondName;
             let password = this.user.password;
+            let birthDate = this.user.birthDate;
+            let nationality = this.user.nationality;
+            let gender = this.user.gender;
             const id = this.$store.state.user_id;
             const URL = "http://localhost:5000/users/" + id ;
             const TOKEN = this.$store.state.TOKEN;
             axios
                 .patch(
                     URL,
-                    { fisrtName: this.fisrtName, secondName: secondName, password: password },
+                    { firstName: firstName, secondName: secondName, password: password, birthDate: birthDate,
+                    nationality: nationality, gender:gender},
                     { headers: { "Content-Type": "application/json", Authorization: `Bearer ${TOKEN}`},}
                     )
                 .then((res) => {
@@ -74,8 +92,12 @@ export default {
         onReset(event) {
             event.preventDefault();
             // Reset our form values
-            this.user.userName = "";
+            this.user.fisrtName = "";
+            this.user.secondName = "";
             this.user.password = "";
+            this.user.nationality = "";
+            this.user.gender = "";
+            this.user.birthDate = "";
         },
     },
     computed: {
@@ -91,5 +113,21 @@ export default {
 .main{
     background-color: rgb(4, 4, 40);
     height: 100vh;
+}
+.hmm {
+    position: relative;
+    background: #c0deff;
+    top: 40px;
+    border-radius: 25px;
+}
+
+.Formm {
+    text-align: center;
+    margin-top: 50px;
+    color: rgba(0, 0, 0, 0.678);
+}
+.btnn {
+    margin:25px ;
+    width: 60%;
 }
 </style>
